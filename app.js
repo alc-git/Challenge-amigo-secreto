e9593185c33a13a8e24ac2f67f7223b7d250a85e
Alexander <alexander.alc.93@gmail.com>
2025-08-12 12:11:29 -0400

    Primer commit:Amigo secreto

---
+++ app.js
@@ -0,0 +1,13 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigos(){
+   document.getElementById = "amigo"; 
+
+}
+
+console.log();
+alert("Por favor, inserte un nombre.");
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}

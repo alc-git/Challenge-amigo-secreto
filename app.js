0b6b6d577e88a6de3d81b9bb5473cb40c9cba263
Alexander <alexander.alc.93@gmail.com>
2025-08-12 22:25:33 -0400

    Proyecto con todas las funciones

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let amigos = [];
 
-function agregarAmigos(){
-   document.getElementById = "amigo"; 
 
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value.trim();
+
+    
+    if (nombre === "")
+    {
+        alert("Por favor escribe un nombre");
+        return;
+    }
+
+
+      let soloLetras = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+      if (!soloLetras.test(nombre)) {
+        alert("Solo se permiten letras y espacios.");
+        return;
+      }
+
+    amigos.push(nombre);
+    document.getElementById("amigo").value = "";
+    mostrarLista();
 }
 
-console.log();
-alert("Por favor, inserte un nombre.");
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
+
+function mostrarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++)
+    {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+}
+
+
+function sortearAmigo() {
+    if (amigos.length < 2)
+    {
+        alert("Debes ingresar al menos 2 nombres");
+    }
+    else
+    {
+        let aleatorio = Math.floor(Math.random() * amigos.length);
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = "";
+
+        let item = document.createElement("li");
+        item.textContent = "El amigo secreto es: " + amigos[aleatorio];
+        resultado.appendChild(item);
+    }
 }
